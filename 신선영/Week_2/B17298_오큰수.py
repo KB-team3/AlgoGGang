d164cda940f2642a8912fb5d0b20ae95afc9de06
SY <97646668+drsuneamer@users.noreply.github.com>
2023-04-27 15:46:45 +0900

    [CORRECT] 주석 일부 수정

--- 신선영/Week_2/B17298_오큰수.py
+++ 신선영/Week_2/B17298_오큰수.py
@@ -14,13 +14,13 @@ for i in range(N - 1, -1, -1):  # 마지막부터 탐색
         T.append(L[i])  # 현재 값 후보로 저장
 
     else:
-        while T and T[-1] <= L[i]:  # 더 큰 값을 찾을 때까지 후보 없애버림
+        while T and T[-1] <= L[i]:  # 더 큰 값을 찾을 때까지 후보 목록에서 삭제하며 확인
             T.pop(-1)
         if T:
             A.append(T[-1])   # 찾으면 마주한 값을 답에 추가
         else:
             A.append(-1)  # 후보중에 더 큰 값이 없으면 -1
-        T.append(L[i])  # 현재 내 값도 후보에 부조건 추가
+        T.append(L[i])  # 현재 내 값도 후보에 무조건 추가
 
 for i in range(N - 1, -1, -1):  # 답 순서 반대로 출력
     print(A[i], end=" ")
