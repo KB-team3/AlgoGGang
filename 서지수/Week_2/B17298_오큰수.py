956af3c4c2c192914dbef402a4b7152692c8f733
Seojisoo20191941 <amy0480@naver.com>
2023-05-01 09:39:04 +0900

    [ADD] B17928_오큰수 풀이

--- 서지수/Week_2/B17298_오큰수.py
+++ 서지수/Week_2/B17298_오큰수.py
@@ -2,6 +2,21 @@ import sys
 input=sys.stdin.readline
 
 N=int(input())
-graph=(list(map(int, input().split())))
+A=(list(map(int, input().split())))
 
-print(N, graph)
+result = [-1] * N
+stack = []
+
+
+stack.append(0)
+for i in range(1, N):
+    while stack:
+        if A[stack[-1]] >= A[i]:
+            break
+        result[stack.pop()] = A[i]
+    stack.append(i)
+
+for i in range(len(result)):
+    print(result[i], end=" ")
+
+print()
