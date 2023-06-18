d4465458b5f0b6c18c2e1b93e2e071f7f72ea3f6
rninji <yellowminji@naver.com>
2023-06-19 02:55:03 +0900

    [ADD] B9081_단어맞추기

---
+++ 길민지/Week_8/B9081_단어맞추기.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class B9081_단어맞추기 {
+    static int N;
+    static String input[];
+    public static void init(){
+        Scanner sc = new Scanner(System.in);
+        N = Integer.parseInt(sc.nextLine());
+
+        input = new String[N];
+        for(int i=0; i<N; i++){
+            input[i] = sc.nextLine();
+        }
+    }
+    public static void solution(){
+        for(String s:input){
+            boolean isLast = true;
+            char[] word = s.toCharArray();
+
+            int now = word.length-1;
+            for(int i=word.length-2; i>=0; i--){ // 뒤에서부터 체크
+                if(word[i]<word[now]){ // 감소 시
+                    isLast = false;
+                    char temp = word[i];
+                    for(int j=word.length-1; j>i; j--){
+                        if(word[j]>temp){ // 바꿀 단어보다 더 큰 단어 나오면 교체
+                            word[i] = word[j];
+                            word[j] = temp;
+                            break;
+                        }
+                    }
+                    char[] sort = Arrays.copyOfRange(word, i+1, word.length);
+                    Arrays.sort(sort); // 뒷부분 정렬
+                    System.arraycopy(sort, 0, word, i+1, word.length-1-i);
+                    String rs = new String(word);
+                    System.out.println(rs); // 출력
+                    break;
+                } else {
+                    now = i;
+                }
+            }
+            if(isLast) System.out.println(s); // 마지막 단어인 경우
+        }
+    }
+
+    public static void main(String args[]){
+        init();
+        solution();
+    }
+}
