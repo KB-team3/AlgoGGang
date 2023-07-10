b7e9395da3dcaac9691466e0cd381b04384d6d7d
rninji <yellowminji@naver.com>
2023-07-10 23:40:27 +0900

    [MOD] P72411_메뉴리뉴얼 조합 수정

--- 길민지/Week_11/P72411_메뉴리뉴얼.java
+++ 길민지/Week_11/P72411_메뉴리뉴얼.java
@@ -1,21 +1,21 @@
 import java.util.*;
-
-public class P72411_메뉴리뉴얼 {
+class Solution {
+    
     public static Map<String, Integer>[] mapArr;
     public static int N;
     public static List<String> list;
 
-    public static void solution(String[] orders, int[] course){
+    public static void sol(String[] orders, int[] course){
 
-            for(int i=0; i<course.length; i++){
-                int r = course[i];
-                mapArr[i] = new HashMap<>();
-                for(String order : orders){
-                //nCr 구하기
-                char[] c = new char[r];
-                combination(order, 0,0, r, c, mapArr[i]);
-                }
+        for(int i=0; i<course.length; i++){
+            int r = course[i];
+            mapArr[i] = new HashMap<>();
+            for(String order : orders){
+            //nCr 구하기
+            char[] c = new char[r];
+            combination(order, 0,0, r, c, mapArr[i]);
             }
+        }
 
         for(int i=0; i<course.length; i++){
             int max = 2;
@@ -28,11 +28,13 @@ public class P72411_메뉴리뉴얼 {
         }
         Collections.sort(list);
     }
-    public static void combination(String order, int i, int n, int r, char[] c, Map<String, Integer> map){ // 코스, 배열 인덱스, 위치, 배열 크기, 배열
+    
+    public static void combination(String order, int i, int n, int r, char[] c, Map<String, Integer> map){ 
+        // 코스, 배열 인덱스, 위치, 배열 크기, 배열
         if(i==r){
-            Arrays.sort(c);
-            String s = new String(c);
-            System.out.println(s);
+            char[] newArr = c.clone();
+            Arrays.sort(newArr);
+            String s = new String(newArr);
             if(map.containsKey(s)){
                 map.put(s, map.get(s)+1);
             }
@@ -48,16 +50,19 @@ public class P72411_메뉴리뉴얼 {
         c[i] = order.charAt(n);
         combination(order, i+1, n+1, r, c, map);
     }
-
-    public static void main(String args[]){
-        String orders [] = {"XYZ", "XWY", "WXA"};
-        int course [] = {2,3,4};
+    
+    public String[] solution(String[] orders, int[] course) {
         mapArr = new Map[course.length];
         list = new ArrayList<>();
-        solution(orders, course);
+        sol(orders, course);
+        
+        System.out.println(list);
+        
         String[] answer = list.stream()
-                .toArray(String[]::new);
-
-        System.out.println(Arrays.toString(answer));
+                            .toArray(String[]::new);
+        return answer;
     }
 }
+
+
+
