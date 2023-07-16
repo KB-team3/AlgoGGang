f91545836abf3a5a7f19cbedac29320a8d6179c7
rninji <yellowminji@naver.com>
2023-07-17 02:28:17 +0900

    [ADD] P155651_호텔대실

---
+++ 길민지/Week_12/P155651_호텔대실.py
@@ -0,0 +1,17 @@
+def solution(book_time):
+    time = [0] * (24 * 60)
+
+    # 배열로 전환
+    book = []*len(book_time)
+    for b in book_time:
+        in_time = list(map(int, b[0].split(":")))
+        out_time = list(map(int, b[1].split(":")))
+        book.append([in_time[0] * 60 + in_time[1], out_time[0] * 60 + out_time[1] + 10])
+    # 대실 시작 오름차순 정렬
+    book.sort()
+
+    for b in book :
+        for i in range (b[0], b[1] if b[1]<1440 else 1440):
+            time[i]+=1
+
+    return max(time)
