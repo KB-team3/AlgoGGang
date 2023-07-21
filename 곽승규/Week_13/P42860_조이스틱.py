448936a68cb88b377fd18972f90d700c4f9df26d
Gyu <tmdrb0912@naver.com>
2023-07-21 10:40:54 +0900

    [ADD] P42860_조이스틱

---
+++ 곽승규/Week_13/P42860_조이스틱.py
@@ -0,0 +1,21 @@
+def solution(name):
+    answer = 0 # 조이스틱 조작횟수
+    
+    minMove = len(name) - 1 # 기본 최소 좌우이동 횟수는 길이 - 1
+    
+    for i, char in enumerate(name):
+        # 해당 알파벳 변경 최솟값 추가
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
+        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+    
+        # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
+        minMove = min([minMove, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
+        
+    # 알파벳 변경(상하이동) 횟수에 좌우이동 횟수 추가
+    answer += minMove
+    
+    return answer
